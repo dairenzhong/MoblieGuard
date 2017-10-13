328f93cd333402e484881187161965beb64e2b9c
dairenzhong <895500138@qq.com>
2017-10-13 16:57:17 +0800

    10.13 16:56

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetUpPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
@@ -106,13 +107,14 @@ public class HomeActivity extends AppCompatActivity{
                 @Override
                 public void confirm() {
                     if (TextUtils.isEmpty(mInPswDialog.getPassword())){
-                        Toast.makeText(HomeActivity.this,"密码不能为空！", 0).show();
+                        Toast.makeText(HomeActivity.this,"密码不能为空！",Toast.LENGTH_LONG).show();
                     }else if(password.equals(MD5Utils.encode(mInPswDialog.getPassword()))){
                         mInPswDialog.dismiss();
+                        startActivity(LostFindActivity.class);
                         Toast.makeText(HomeActivity.this,"可以进入手机防盗模块",Toast.LENGTH_LONG).show();
                     }else {
                         mInPswDialog.dismiss();
-                        Toast.makeText(HomeActivity.this,"密码有误，请重新输入", 0).show();
+                        Toast.makeText(HomeActivity.this,"密码有误，请重新输入", Toast.LENGTH_LONG).show();
                     }
                 }
 
