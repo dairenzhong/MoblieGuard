8ab8f250dc5b1af850da6c99c3cd03bf80bd40ab
dairenzhong <895500138@qq.com>
2017-10-13 10:42:25 +0800

    10.13 10:42

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/MD5Utils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/MD5Utils.java
@@ -1,21 +1,31 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard.utils;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.view.View;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Administrator on 2017/10/11.
  */
 
-public class MD5Utils extends Dialog implements View.OnClickListener {
-    public MD5Utils(@NonNull Context context) {
-        super(context);
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
+ public class MD5Utils{
+    public  static String encode(String text) {
+        try{
+        MessageDigest digest = MessageDigest.getInstance("md5");
+        byte[] result = digest.digest(text.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for(byte b : result){
+            int number = b&0xff;
+            String hex = Integer.toHexString(number);
+            if (hex.length()==1){
+                sb.append("0"+hex);
+            }else{
+                sb.append(hex);
+            }
+        }
+        return sb.toString();
+    }catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+        }
 }
